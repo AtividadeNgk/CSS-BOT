bd33387b509f9067a40b5cb880c1166f8d0c068a
AtividadeNgk <goatpaydev@gmail.com>
2025-07-08 16:23:22 -0300

    app.py

--- app.py
+++ app.py
@@ -475,10 +475,10 @@ async def start_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # Botões do menu
     keyboard = [
-        [InlineKeyboardButton("𝗖𝗮𝗱𝗮𝘀𝘁𝗿𝗮𝗿 𝗕𝗼𝘁", callback_data="registro_cadastrar")],
+        [InlineKeyboardButton("📦 𝗖𝗮𝗱𝗮𝘀𝘁𝗿𝗮𝗿 𝗕𝗼𝘁", callback_data="registro_cadastrar")],
         [
-            InlineKeyboardButton("𝗠𝗲𝘂𝘀 𝗕𝗼𝘁𝘀", callback_data="registro_ver_bots"),
-            InlineKeyboardButton("𝗧𝗿𝗼𝗰𝗮𝗿 𝗧𝗼𝗸𝗲𝗻", callback_data="registro_substituir")
+            InlineKeyboardButton("👤 𝗠𝗲𝘂𝘀 𝗕𝗼𝘁𝘀", callback_data="registro_ver_bots"),
+            InlineKeyboardButton("♻️ 𝗧𝗿𝗼𝗰𝗮𝗿 𝗧𝗼𝗸𝗲𝗻", callback_data="registro_substituir")
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
